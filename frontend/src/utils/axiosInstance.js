a081389cf6ac5fba089a87a78ec081636f0182c5
Antika Noor <antika.noor@bjitacademy.com>
2023-10-30 14:14:41 +0600

    aws

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://35.180.58.74:8000",
     timeout: 10000
 })
 
 const axiosInstanceToken = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://35.180.58.74:8000",
     timeout: 10000
 })
 
